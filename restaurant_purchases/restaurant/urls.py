6fd7447e7241be3e6d1c41a30a8ee2ceaa9b12df
Vadim <vs.sat.dev@gmail.com>
2022-05-25 16:42:12 +0500

    Restaurant App. Ingridient List and MenuItem List pages created.

--- restaurant_purchases/restaurant/urls.py
+++ restaurant_purchases/restaurant/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import IngridientList, MenuItemList
+
 
 app_name = 'restaurant'
 
 urlpatterns = [
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('ingridient-list/', IngridientList.as_view(), name='ingridient-list'),
+    path('menu-item-list/', MenuItemList.as_view(), name='menu-item-list'),
 ]
--- restaurant_purchases/restaurant/views.py
+++ restaurant_purchases/restaurant/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-from .models import Ingridient
+from .models import Ingridient, MenuItem, RecipeRequirement, Purchase
+from .forms import IngridientForm, MenuItemForm
 
 
+@method_decorator(login_required, name='dispatch')
 class IngridientList(ListView):
     model = Ingridient
     paginate_by = 3
     template_name = 'ingridient-list.html'
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['form'] = IngridientForm()
+         return context
+    
+    def post(self, request):
+        form = IngridientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return super().get(request)
+        else:
+            context = {'errors': form.errors, 'form': form}
+            return render(request, 'ingridient-post.html', context=context)
+
+
+@method_decorator(login_required, name='dispatch')
+class MenuItemList(ListView):
+    model = MenuItem
+    paginate_by = 3
+    template_name = 'menu-item-list.html'
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['form'] = MenuItemForm()
+         return context
+    
+    def post(self, request):
+        form = MenuItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return super().get(request)
+        else:
+            context = {'errors': form.errors, 'form': form}
+            return render(request, 'menu-item-post.html', context=context)
