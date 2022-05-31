39ae4edea3e4fba2d4c52cfba9245a5b0b578606
Vadim <vs.sat.dev@gmail.com>
2022-05-31 16:14:37 +0500

    heroku deploy commit2

--- restaurant_purchases/accounts_jwt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsJWTConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts_jwt'
--- restaurant_purchases/restaurant/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Ingridient, MenuItem, RecipeRequirement, Purchase
-
-
-admin.site.register(Ingridient)
-admin.site.register(MenuItem)
-admin.site.register(RecipeRequirement)
-admin.site.register(Purchase)
