967843682531ee9a505175c5cef5287800173334
Vadim <vs.sat.dev@gmail.com>
2022-05-31 12:16:45 +0500

    Add DRF registration and JWT login/logout

---
+++ restaurant_purchases/accounts_jwt/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountsJWTConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts_jwt'
--- restaurant_purchases/restaurant_purchases/urls.py
+++ restaurant_purchases/restaurant_purchases/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('accounts_jwt/', include('accounts_jwt.urls')),
     path('restaurant/', include('restaurant.urls')),
 ]
