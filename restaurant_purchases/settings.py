d8a257da56f2d778fc1e49e68950266595cfd0e8
Vadim <vs.sat.dev@gmail.com>
2022-06-01 13:33:07 +0500

    Add secret files

--- restaurant_purchases/settings.py
+++ restaurant_purchases/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,9 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-purchases.herokuapp.com']
+#ALLOWED_HOSTS = ['restaurant-purchases.herokuapp.com']
+ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -87,10 +90,23 @@ WSGI_APPLICATION = 'restaurant_purchases.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': '.pg_service',
+            'passfile': '.my_pgpass',
+        },
+    }
+}
+
+"""DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
+
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
 
 
 # Password validation
