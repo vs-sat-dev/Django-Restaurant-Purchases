0c871beef2e77ab26d6a0e23f1cd5f54f37f1085
Vadim <vs.sat.dev@gmail.com>
2022-06-03 09:24:34 +0500

    Change home url 9

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import (
     IngridientList, MenuItemList, MenuItemDetail, RecipeRequirementList, MenuItemBuy,
@@ -18,4 +19,5 @@ urlpatterns = [
     path('recipe-requirement-list/', RecipeRequirementList.as_view(), name='recipe-requirement-list'),
     path('purchase-list/', PurchaseList.as_view(), name='purchase-list'),
     path('purchase-cost/', PurchaseCost.as_view(), name='purchase-cost'),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
