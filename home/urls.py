e7b40cbc500f444123093bc20f89ac89592b6cfa
Vadim <vs.sat.dev@gmail.com>
2022-06-03 09:21:21 +0500

    Change home url 7

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
