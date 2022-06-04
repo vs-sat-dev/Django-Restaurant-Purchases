60d0a401e63c345774093fb9ca4c0d1fe82cd641
Vadim <vs.sat.dev@gmail.com>
2022-06-04 14:49:43 +0500

    CI/CD on VPS 20

--- restaurant_purchases/settings.py
+++ restaurant_purchases/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restaurant-purchases.herokuapp.com', '127.0.0.1', '68.183.201.244']
 
