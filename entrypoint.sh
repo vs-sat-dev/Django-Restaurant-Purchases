103102073a3176826944afb359f1843e0a0e098c
Vadim <vs.sat.dev@gmail.com>
2022-06-07 10:54:54 +0500

    Test docker functional 57

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate
-python manage.py collectstatic
-gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000 --daemon
+#python manage.py collectstatic
+#gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000 --daemon
 #gunicorn restaurant_purchases.wsgi:application --bind 68.183.201.244:8000
