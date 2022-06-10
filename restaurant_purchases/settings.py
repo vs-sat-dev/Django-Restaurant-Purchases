2edc26a28123467e3f31b0ec75c1689c1efa82cb
Vadim <vs.sat.dev@gmail.com>
2022-06-10 12:01:12 +0500

    Telegram Bot API 18

--- restaurant_purchases/settings.py
+++ restaurant_purchases/settings.py
@@ -34,8 +34,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['restaurant-purchases.herokuapp.com', '127.0.0.1', 
                 '68.183.201.244', '134.122.43.197', '0.0.0.0', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['restaurant-purchases.herokuapp.com', '127.0.0.1', 
-                '68.183.201.244', '134.122.43.197', '0.0.0.0', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['http://restaurant-purchases.herokuapp.com', 'http://127.0.0.1', 
+                'http://68.183.201.244', 'http://134.122.43.197', 'http://0.0.0.0', 'http://localhost',
+                'http://api.telegram.org']
 
 # Application definition
 
