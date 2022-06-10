4428ebf9847b169ff842852d0730c6e776d525b2
Vadim <vs.sat.dev@gmail.com>
2022-06-10 12:30:24 +0500

    Telegram Bot API 20

--- restaurant/dispatcher.py
+++ restaurant/dispatcher.py
@@ -1,12 +1,15 @@
 import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
+
+status = 'idle'
 
 
 def start(update, _):
@@ -20,6 +23,15 @@ def start(update, _):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Do you wish add ingredient?:', reply_markup=reply_markup)
 
+async def custom_command(update: Update, context: ContextTypes) -> None:
+    text = update.message.text
+    if status == 'add_ingredient':
+        try:
+            text = float(update.message.text)
+        except:
+            text = 'Quantity must be numeric'
+    await update.message.reply_text(text)
+
 
 def button(update, _):
     query = update.callback_query
@@ -32,7 +44,9 @@ def button(update, _):
     query.answer()
     # редактируем сообщение, тем самым кнопки 
     # в чате заменятся на этот ответ.
-    query.edit_message_text(text=f"Выбранный вариант: {variant}")
+    if variant == 'yes':
+        status = 'add_ingredient'
+        query.edit_message_text(text=f"How much ingredient must be bouth?")
 
 def help_command(update, _):
     update.message.reply_text("Используйте `/start` для тестирования.")
@@ -44,6 +58,7 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, custom_command))
 
     # Запуск бота
     updater.start_polling()
