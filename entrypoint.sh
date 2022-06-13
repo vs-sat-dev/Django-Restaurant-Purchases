79d438d7bf18db4bcc486b2103cadf891420e7c3
Vadim <vs.sat.dev@gmail.com>
2022-06-13 16:42:50 +0500

    Telegram Bot API 35

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 python manage.py makemigrations
 python manage.py migrate
 python telegram_bot.dispatcher.py &
-echo "from django.contrib.auth.models import User; User.objects.create_superuser(`admin`,`admin@example.com`,`admin`)" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser(admin,admin@example.com,admin)" | python manage.py shell
 #python manage.py collectstatic
 gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000
 #gunicorn restaurant_purchases.wsgi:application --bind 68.183.201.244:8000
