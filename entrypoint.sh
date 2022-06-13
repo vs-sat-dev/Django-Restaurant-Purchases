331028a990c03e22d9f065e4f8818f995f8d434f
Vadim <vs.sat.dev@gmail.com>
2022-06-13 16:24:25 +0500

    Telegram Bot API 31

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-cd telegram_bot
-python dispatcher.py &
-cd ..
+python telegram_bot.dispatcher.py &
 #python manage.py collectstatic
 gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000
 #gunicorn restaurant_purchases.wsgi:application --bind 68.183.201.244:8000
