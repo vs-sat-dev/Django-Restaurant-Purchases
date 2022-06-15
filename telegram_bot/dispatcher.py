9de24480fd00b082be76a114ef450a8c2e6d5089
Vadim <vs.sat.dev@gmail.com>
2022-06-15 16:15:25 +0500

    Telegram Webhook 3

--- telegram_bot/dispatcher.py
+++ telegram_bot/dispatcher.py
@@ -31,9 +31,13 @@ if __name__ == '__main__':
     job_minute = jq.run_repeating(callback_minute, interval=5)
 
     #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                       port=8443,
+                       url_path=api_token)
+    updater.bot.setWebhook(f'https://134.122.43.197:8443/{api_token}')
 
-    updater.start_webhook(listen='0.0.0.0', port=8443, url_path='TOKEN',
-                          key='private.key', cert='cert.pem',
-                          webhook_url=f'https://134.122.43.197:8443/{api_token}')
+    #updater.start_webhook(listen='0.0.0.0', port=8443, url_path='TOKEN',
+    #                      key='private.key', cert='cert.pem',
+    #                      webhook_url=f'https://134.122.43.197:8443/{api_token}')
 
     updater.idle()
