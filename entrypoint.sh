a2cb3498552018b75e2c8bf622801edef26d6ccb
Vadim <vs.sat.dev@gmail.com>
2022-06-18 09:52:07 +0500

    Telegram Webhook 51

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ python manage.py makemigrations
 python manage.py migrate
 python telegram_bot/dispatcher.py &
 #python manage.py collectstatic
-gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000 -d
+gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000 --daemon
 #gunicorn restaurant_purchases.wsgi:application --bind 68.183.201.244:8000
