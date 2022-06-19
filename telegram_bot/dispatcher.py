cf917594fbd8ee9c52f1598994b2185173932e2c
vs-sat-dev <vs.sat.dev@gmail.com>
2022-06-19 10:26:12 +0500

    Telegram Webhook 134

--- telegram_bot/dispatcher.py
+++ telegram_bot/dispatcher.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     #updater.start_polling()
     try:
-        updater.start_webhook(listen="0.0.0.0",
+        updater.run_webhook(listen="127.0.0.1",
                         port=5000,
                         url_path=api_token, cert='fullchain.pem',# key='privkey.pem',
                         webhook_url=f'https://chupakabra.monster/{api_token}')
