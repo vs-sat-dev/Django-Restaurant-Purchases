4275f1ca7054059a74af97829244df880a8422d6
Vadim <vs.sat.dev@gmail.com>
2022-06-22 10:48:54 +0500

    Docker test 8

--- restaurant_purchases/settings.py
+++ restaurant_purchases/settings.py
@@ -108,10 +108,10 @@ DATABASES = {
     }
 }
 
-CELERY_BROKER='redis://redis:6379/0'
-CELERY_BACKEND='redis://redis:6379/0'
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
+#CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
+#CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
 """DATABASES = {
     'default': {
