64505cd3aba25f7ce2d25b6c05a858b3b2fb1b81
Vadim <vs.sat.dev@gmail.com>
2022-06-22 16:02:00 +0500

    Docker test 46

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py migrate
 python telegram_bot/dispatcher.py &
 celery -A restaurant_purchases worker -l INFO &
 #celery -A restaurant_purchases flower --address=0.0.0.0 --port=5566 &
-celery --workdir=restaurant_purchases/  --app=restaurant_purchases.celery:app flower
+celery --app=restaurant_purchases.celery:app flower
 #celery flower --url_prefix=flower &
 #python manage.py collectstatic
 gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000
