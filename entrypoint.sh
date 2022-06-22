5841085c6c3998cd69ebc16aedf538430a9ade44
Vadim <vs.sat.dev@gmail.com>
2022-06-22 12:05:33 +0500

    Docker test 20

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 python manage.py makemigrations
 python manage.py migrate
 python telegram_bot/dispatcher.py &
-celery -A restaurant_purchases worker -l INFO
+celery -A restaurant_purchases worker -l INFO &
 #python manage.py collectstatic
 gunicorn restaurant_purchases.wsgi:application --bind 0.0.0.0:8000
 #gunicorn restaurant_purchases.wsgi:application --bind 68.183.201.244:8000
