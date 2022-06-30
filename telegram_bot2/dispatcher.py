7278227c86be468197a83d2c4db716c69386977e
Vadim <vs.sat.dev@gmail.com>
2022-06-30 17:17:33 +0500

    Telegram test 6

--- telegram_bot2/dispatcher.py
+++ telegram_bot2/dispatcher.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     #updater.start_polling()
     updater.start_webhook(listen="0.0.0.0", port=5001, url_path=api_token,# cert='fullchain.pem',# key='privkey.pem',
                           webhook_url=f'https://chupakabra.monster/{api_token}')
-    with open('tbot.info', 'w') as f:
+    with open('tbot2.info', 'w') as f:
         f.write(str(telegram.Bot(api_token).get_webhook_info()))
     #updater.bot.setWebhook(f'https://chupakabra.monster/{api_token}', certificate=open('fullchain.pem', 'rb'))
     #updater.bot.setWebhook(f'https://134.122.43.197:8443/{api_token}')
